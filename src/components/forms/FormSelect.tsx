5506b57b232928a08b890d5739676ca1ff4f189b
Enma586 <enmaflore7421@gmail.com>
2026-04-26 13:55:00 -0600

    add form select component

---
+++ src/components/forms/FormSelect.tsx
@@ -0,0 +1,63 @@
+import type { Control, FieldPath, FieldValues } from 'react-hook-form';
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface FormSelectProps<T extends FieldValues> {
+  name: FieldPath<T>;
+  control: Control<T>;
+  label: string;
+  options: Option[];
+  placeholder?: string;
+}
+
+export function FormSelect<T extends FieldValues>({
+  name,
+  control,
+  label,
+  options,
+  placeholder = 'Seleccionar...',
+}: FormSelectProps<T>) {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue placeholder={placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              {options.map((opt) => (
+                <SelectItem key={opt.value} value={opt.value}>
+                  {opt.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
