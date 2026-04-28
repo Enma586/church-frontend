e257331ef47f4385b7d57515cd5410782e9790c6
Enma586 <enmaflore7421@gmail.com>
2026-04-28 11:01:56 -0600

    add notification in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 import { Provider } from 'react-redux';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
-import { store } from './store';
-import { queryClient } from './lib/query-client';
-import { router } from './routes';
+import { store } from '@/store';
+import { queryClient } from '@/lib/query-client';
+import { router } from '@/routes';
+import { ToastProvider } from '@/components/feedback/ToastProvider';
 
-
-// 1. Importamos el proveedor de tooltips de tu carpeta UI
+// 1. Importamos el proveedor de tooltips de shadcn
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { ToastProvider } from './components/feedback/ToastProvider';
 
-function App() {
+export default function App() {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        {/* 2. Envolvemos el Router y el Toaster con el TooltipProvider */}
+        {/* 2. Envolvemos el Router y el Toast con el TooltipProvider */}
         <TooltipProvider>
           <RouterProvider router={router} />
-          <ToastProvider/>
+          <ToastProvider /> 
         </TooltipProvider>
       </QueryClientProvider>
     </Provider>
   );
 }
-
-export default App;
